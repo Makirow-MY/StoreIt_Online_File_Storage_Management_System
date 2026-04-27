8de6b8155a3c3ff2b1b5e5c2d2ec6305740d832e
Makirow-Makia Yengue <godwillmakiayengue@gmail.com>
2026-04-28 00:59:59 +0100

    Refactor user authentication handling in Layout
    Removed useEffect for redirecting unauthenticated users and handled redirection directly in the Layout function.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,7 @@
-"use client"
 import React from "react";
 import Sidebar from "@/components/Sidebar";
 import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
-import { useEffect } from "react";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect} from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,15 +10,8 @@ export const dynamic = "force-dynamic";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
-  //const router = useRouter();
-
-  useEffect(() => {
-      if (!currentUser)  {
-      redirect("/sign-in")
-      }
-    },[currentUser])
-  
-  //if (!currentUser) return redirect("/sign-in");
+ 
+  if (!currentUser) return redirect("/sign-in");
 
   return (
     <main className="flex h-screen">
