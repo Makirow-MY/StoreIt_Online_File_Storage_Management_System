025844a49dd56188d59e3542eff14a5a14e18bca
Makirow-Makia Yengue <godwillmakiayengue@gmail.com>
2026-04-28 00:51:03 +0100

    Remove 'use server' directive from sign-out form
    Removed unnecessary 'use server' directive from the sign-out form action.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+"use server";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -19,8 +20,6 @@ const Header = ({
         <FileUploader ownerId={userId} accountId={accountId} />
         <form
           action={async () => {
-            "use server";
-
             await signOutUser();
           }}
         >
